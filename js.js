ec53f356d524422c0304ac0b943061130267a101
DeepSantinity <santymunoz850@gmail.com>
2026-05-12 21:00:46 -0300

    Desafio 2-3

--- js.js
+++ js.js
@@ -1,25 +1,33 @@
-fetch("https://rickandmortyapi.com/api/character")
-.then(respuesta => respuesta.json())
-.then(datos => {
-datos.results.forEach(personaje => {
-document.getElementById("personajes").innerHTML += `
-<div class="col-md-3 mb-4">
-<div class="card">
-<img src="${personaje.image}" class="card-img-top">
-<div class="card-body">
-<h5 class="card-title">
-${personaje.name}
-</h5>
-<p>
-${personaje.species}
-</p>
-<p>
-${personaje.status}
-</p>
-</div>
-</div>
-</div>
+function traerPersonaje() {
 
-`;
-});
-});
+  fetch("https://rickandmortyapi.com/api/character")
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+
+      const random = Math.floor(Math.random() * datos.results.length);
+
+      const personaje = datos.results[random];
+
+      document.getElementById("personajes").innerHTML = `
+        <div class="col-md-4">
+          <div class="card">
+
+            <img src="${personaje.image}" class="card-img-top">
+
+            <div class="card-body">
+
+              <h5 class="card-title">
+                ${personaje.name}
+              </h5>
+
+              <p>${personaje.species}</p>
+
+              <p>${personaje.status}</p>
+
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+}
